a85cafd84c4f1abefd779a5179620c8304860cd8
prkrgreenwell <prkrgreenwell@gmail.com>
2022-10-18 15:09:12 -0600

    rows are dynamically colored based on time

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 var now = dayjs().format("dddd MMM D, YYYY");
 
 $("#currentDay").text(now);
+
+var hour = dayjs().hour();
 console.log(now);
+console.log(hour);
+
+var hourRows = [
+  $("#nine"),
+  $("#ten"),
+  $("#eleven"),
+  $("#twelve"),
+  $("#one"),
+  $("#two"),
+  $("#three"),
+  $("#four"),
+  $("#five"),
+];
+
+console.log(hourRows);
+
+function colorRows() {
+  var colorVar = hour - 9;
+  for (var i = 0; i < hourRows.length; i++) {
+    if (colorVar > i) {
+      hourRows[i].addClass("past");
+    } else if (colorVar < i) {
+      hourRows[i].addClass("future");
+    } else {
+      hourRows[i].addClass("present");
+    }
+  }
+}
+
+colorRows();
